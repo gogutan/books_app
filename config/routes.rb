71ff29e05ecf1ac64f03f85ea5d844e53c963045
gogutan <goaglia@gmail.com>
2020-01-10 14:01:53 +0900

    create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "books#index"
   scope "(:locale)" do
     resources :books
+    resources :users, only: [:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
