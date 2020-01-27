aa25cd288af4c05e13a62ea4947de48d36167a1e
gogutan <50437473+gogutan@users.noreply.github.com>
2020-01-27 23:49:44 +0900

    relate user to relationship
    UserモデルとRelationshipモデルを関連付けて、Userモデルに follow, unfollow, following? の3メソッドを追加しました。また、Userのサンプルデータ生成コードを seeds.rb に追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
   has_one_attached :icon
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def self.find_for_github_oauth(auth, signed_in_resource = nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
@@ -21,5 +27,17 @@ class User < ApplicationRecord
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザー
+(1..100).each do |i|
+  email = "example-#{i}@example.com"
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               provider: "",
+               uid: User.create_unique_string)
+end
+
+# リレーションシップ
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
